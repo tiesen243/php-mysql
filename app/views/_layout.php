12e14e2adcb9a95ef0f56d8822c59c4947775b99
tiesen243 <ttien56906@gmail.com>
2025-08-06 23:11:00 +0700

    chore: clean

--- app/views/_layout.php
+++ app/views/_layout.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title ?? 'PHP MVC App' ?></title>
-    <meta name="description" content="<?= $description ?? 'A simple PHP MVC application' ?>">
-    <link href="/css/globals.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?= $title ?? 'PHP MVC App' ?></title>
+  <meta name="description" content="<?= $description ?? 'A simple PHP MVC application' ?>">
+  <link href="/css/globals.css" rel="stylesheet">
 </head>
+
 <body>
   <?= $content ?>
 </body>
--- public/index.php
+++ public/index.php
@@ -1,10 +1,7 @@
 <?php
 
-require_once '../vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-require_once '../core/app.php';
-require_once '../core/controller.php';
+require_once dirname(__DIR__) . '/core/app.php';
 
-$app = new App();
-
-?>
+new App();
